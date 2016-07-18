c48341fe7456345253a503348390ec28722bc9f9
Daiderd Jordan <daiderd@gmail.com>
2016-07-18 09:37:31 +0200

    nodejs: fixed darwin dependencies

--- pkgs/development/web/nodejs/v6.nix
+++ pkgs/development/web/nodejs/v6.nix
@@ -1,16 +1,18 @@
 { stdenv, fetchurl, openssl, python, zlib, libuv, v8, utillinux, http-parser
 , pkgconfig, runCommand, which, libtool
 , callPackage
+, darwin ? null
 }@args:
 
-import ./nodejs.nix (args // rec {
+let
+  inherit (darwin.apple_sdk.frameworks) CoreServices ApplicationServices;
+
+in import ./nodejs.nix (args // rec {
   version = "6.3.0";
-  src = fetchurl {
-    url = "https://nodejs.org/download/release/v${version}/node-v${version}.tar.xz";
-    sha256 = "0b7npvxrby203z59r4jnd2v2x54lg8d2gc96c2gj3zyzzrdh3hk6";
-  };
-  extraConfigFlags = stdenv.lib.optionalString (stdenv.isDarwin) [ "--without-inspector" ];
-  preBuild = stdenv.lib.optionalString (stdenv.isDarwin) ''
+  sha256 = "0b7npvxrby203z59r4jnd2v2x54lg8d2gc96c2gj3zyzzrdh3hk6";
+  extraBuildInputs = stdenv.lib.optionals stdenv.isDarwin
+    [ CoreServices ApplicationServices ];
+  preBuild = stdenv.lib.optionalString stdenv.isDarwin ''
     sed -i -e "s|tr1/type_traits|type_traits|g" \
       -e "s|std::tr1|std|" src/util.h
   '';
