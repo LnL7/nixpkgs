9704942777276e7b09d173213102ae57f8fcb45b
Daiderd Jordan <daiderd@gmail.com>
2016-09-13 22:04:31 +0200

    db: fixes for darwin build with libc++-3.8

--- pkgs/development/libraries/db/db-5.3.nix
+++ pkgs/development/libraries/db/db-5.3.nix
@@ -5,4 +5,11 @@ import ./generic.nix (args // rec {
   sha256 = "0a1n5hbl7027fbz5lm0vp0zzfp1hmxnz14wx3zl9563h83br5ag0";
   extraPatches = [ ./clang-5.3.patch ];
   branch = "5.3";
+  drvArgs = {
+    # This is a workaround to fix the build, dependencies will probably run into the same issue.
+    preFixup = ''
+      substituteInPlace $out/include/db.h \
+        --replace store_db_FIXME store
+    '';
+  };
 })
