f95e06cedee117c2bdee80c4cd2481a86b63a1b6
Daiderd Jordan <daiderd@gmail.com>
2017-03-16 08:48:22 +0100

    qt57-qtmultimedia: fix darwin build

--- pkgs/development/libraries/qt-5/5.7/qtmultimedia.nix
+++ pkgs/development/libraries/qt-5/5.7/qtmultimedia.nix
@@ -1,12 +1,11 @@
-{ qtSubmodule, qtbase, qtdeclarative, pkgconfig
+{ stdenv, qtSubmodule, qtbase, qtdeclarative, pkgconfig
 , alsaLib, gstreamer, gst-plugins-base, libpulseaudio
 }:
 
 qtSubmodule {
   name = "qtmultimedia";
   qtInputs = [ qtbase qtdeclarative ];
-  buildInputs = [
-    pkgconfig alsaLib gstreamer gst-plugins-base libpulseaudio
-  ];
+  buildInputs = [ pkgconfig gstreamer gst-plugins-base libpulseaudio ]
+    ++ stdenv.lib.optional stdenv.isLinux alsaLib;
   qmakeFlags = [ "GST_VERSION=1.0" ];
 }
