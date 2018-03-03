b70c46cfc1a9978ff44b8025993253a6fe82f193
Daiderd Jordan <daiderd@gmail.com>
2018-03-03 12:13:52 +0100

    macos-sdk: 10.10 -> 10.11
    NOTE: This won't work unless apple releases the sources for CoreFoundation.

--- pkgs/os-specific/darwin/apple-sdk/default.nix
+++ pkgs/os-specific/darwin/apple-sdk/default.nix
@@ -4,14 +4,14 @@ let
   # TODO: make this available to other packages and generalize the unpacking a bit
   # from https://gist.github.com/pudquick/ff412bcb29c9c1fa4b8d
   # This isn't needed until we get to SDK 10.11, but that presents other challenges
-  # unpbzx = fetchurl {
-  #   url    = "https://gist.githubusercontent.com/pudquick/ff412bcb29c9c1fa4b8d/raw/24b25538ea8df8d0634a2a6189aa581ccc6a5b4b/parse_pbzx2.py";
-  #   sha256 = "0jgp6qbfl36i0jlz7as5zk2w20z4ca8wlrhdw49lwsld6wi3rfhc";
-  # };
+  unpbzx = fetchurl {
+    url    = "https://gist.githubusercontent.com/pudquick/ff412bcb29c9c1fa4b8d/raw/24b25538ea8df8d0634a2a6189aa581ccc6a5b4b/parse_pbzx2.py";
+    sha256 = "0jgp6qbfl36i0jlz7as5zk2w20z4ca8wlrhdw49lwsld6wi3rfhc";
+  };
 
   # sadly needs to be exported because security_tool needs it
   sdk = stdenv.mkDerivation rec {
-    version = "10.10";
+    version = "10.11";
     name    = "MacOS_SDK-${version}";
 
     # This URL comes from https://swscan.apple.com/content/catalogs/others/index-10.10.merged-1.sucatalog, which we found by:
@@ -20,8 +20,8 @@ let
     #  3. ???
     #  4. Profit
     src = fetchurl {
-      url    = "http://swcdn.apple.com/content/downloads/22/52/031-45139/hcjjv7cm4n6yqk56ict73qqw15ikm5iaql/DevSDK_OSX1010.pkg";
-      sha256 = "08bxa93zw7r4vzs28j9giq2qyk3b68ky6jx1bb9850gflr3nvgq1";
+      url    = "http://swcdn.apple.com/content/downloads/61/58/031-85396/fsu2775ydsciy13wycm3zngxrjcp0eqsl2/DevSDK_OSX1011.pkg";
+      sha256 = "182yh8li653pjrzgk7s2dvsqm7vwkk6ry8n31qqs8c0xr67yrqgl";
     };
 
     buildInputs = [ xar xz cpio python ];
@@ -33,10 +33,11 @@ let
     '';
 
     installPhase = ''
-      start="$(pwd)"
       mkdir -p $out
+      python ${unpbzx} Payload
+      xz -d *.xz
       cd $out
-      cat $start/Payload | gzip -d | cpio -idm
+      cat $NIX_BUILD_TOP/*.cpio | cpio -idm
 
       mv usr/* .
       rmdir usr
